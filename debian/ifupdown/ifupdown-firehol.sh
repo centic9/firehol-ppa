368f1b0d53a788c8b6edb2d4a81a78147ac43cfc
Jerome Benoit <calculus@rezozer.net>
2024-01-24 21:34:39 +0000

    Import Debian changes 3.1.7+ds-3
    firehol (3.1.7+ds-3) unstable; urgency=medium
    .
      * Debianization
        - introduce a START_FIREHOL=AUTO scheme that allows one to handle firehol
          with a third-party like ifupdown (Closes: #993322, #536362, #309198);
        - firehol.NEWS, concisely introduce the START_FIREHOL=AUTO scheme;
        - firehol.README.Debian, describe the START_FIREHOL=AUTO scheme;
        - material to handle firehol via the ifupdown tools, introduce;
        - d/firehol.init, d/p/debianization-source-etc_default.patch,
          set explicitly the START_FIREHOL=NO scheme as the default scheme
          (inconsistency fix) (Closes: 976014);
        - contrib/ipset-apply.sh, now part of firehol-tools (Closes: #1050664);
        - d/control:
          - firehol-{,tools-}doc, fireqos-doc, add Multi-Arch: foreign,
            thanks to Jelmer Vernooĳ <jelmer@debian.org>;
          - debhelper, migrate to debhelper-compat (=13);
          - Standards-Version, bump to 4.6.2 (scripts under /usr/libexec);
          - Dependencies over lsb-base, remove;
        - firehol scripts, now under the /usr/libexec hierarchy;
        - d/copyright:
          - copyright year tuples, refresh.

---
+++ debian/ifupdown/ifupdown-firehol.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+#####################################################################
+## Purpose
+# This file is executed by ifupdown in {pre,post}-{up,down} phases
+# of network interface configuration. It allows ifup(8) and ifdown(8)
+# to pre-load and post-load FireHOL firewalls provided that
+# START_FIREHOL is set to AUTO (or auto) in /etc/default/firehol .
+#
+# /etc/default/firehol is sourced by this file.
+#
+# This file is provided by the firehol package.
+
+#####################################################################
+# Copyright (C) 2024 Jerome Benoit <calculus@debian.org>
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package. If not, see <http://www.gnu.org/licenses/>.
+#
+# On Debian systems, the complete text of the GNU General
+# Public License version 2 can be found in "/usr/share/common-licenses/GPL-2".
+
+
+DAEMON=/usr/sbin/firehol
+NAME="firehol"
+DESC="Firewall"
+
+test -x $DAEMON || exit 0
+
+if [ -n "$IF_IFUPDOWN_FIREHOL_MAINT_DEBUG" ]; then
+	if [ -z "$MODE" -o -z "$PHASE" ]; then
+		case $(dirname "$0") in
+			*/if-pre-up.d)
+					PHASE=pre-up;
+			    MODE=start;
+			    ;;
+			*/if-up.d)
+					PHASE=up;
+			    MODE=start;
+			    ;;
+			*/if-down.d)
+					PHASE=down;
+			    MODE=stop;
+			    ;;
+			*/if-post-down.d)
+					PHASE=post-down;
+			    MODE=stop;
+			    ;;
+		esac
+	fi
+	set -x
+fi
+
+[ "$IFACE" != "lo" ] || exit 0
+
+set -e
+
+START_FIREHOL=NO
+export START_FIREHOL
+if [ -r /etc/default/firehol ]; then
+	if [ -o allexport ]; then
+		. /etc/default/firehol
+	else
+		set -a
+		. /etc/default/firehol
+		set +a
+	fi
+fi
+case "$START_FIREHOL" in
+	AUTO|auto) ;;
+	*) exit 0 ;;
+esac
+
+ifud_firehol_do_forceload () {
+	/usr/sbin/firehol start > /dev/null 2>&1
+	}
+
+ifud_firehol_do_forcestop () {
+	/usr/sbin/firehol stop > /dev/null 2>&1
+	}
+
+case "$MODE" in
+	start)
+		ifud_firehol_do_forceload
+		;;
+	stop)
+		if $(grep -sqm1 '#.*\<IFUPDOWN_COMPATIBLE_FIREHOL_CONFIG_FILE\>' /etc/firehol/firehol.conf)
+		then
+			ifud_firehol_do_forceload
+		else
+			ifud_firehol_do_forcestop
+		fi
+		;;
+	*)
+		echo "$0: unkexpected MODE [$MODE]" >&2
+		exit 1
+		;;
+esac
+
+exit 0
